fcfa0ca809fb146c23a01c2e0ae02e904122139f
Felix <mhfelix31@gmail.com>
2024-09-09 20:58:51 -0300

    feat: added error treatment in task

--- api/tasks.py
+++ api/tasks.py
@@ -8,12 +8,16 @@ load_dotenv()
 
 FHIR_ENDPOINT = os.getenv("FHIR_ENDPOINT", "http://localhost:8080/fhir")
 
-@shared_task
+@shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 7, 'countdown': 10})
 def send_patient_to_fhir(patient_data):
     async def async_post():
         async with httpx.AsyncClient() as client:
-            response = await client.post(f"{settings.FHIR_ENDPOINT}/Patient", json=patient_data)
-            print(f"Sent to FHIR: {response.status_code}")
+            try:
+                response = await client.post(f"{FHIR_ENDPOINT}/Patient", json=patient_data)
+                print(f"Sent to FHIR: {response.status_code}")
+            except Exception as e:
+                print("Connection Failed, Retrying in 10 seconds")
+                raise
 
     import asyncio
     asyncio.run(async_post())
