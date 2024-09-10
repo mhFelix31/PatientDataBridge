eee70954253c5eaf7653a98dca6047bc63d5398e
Felix <mhfelix31@gmail.com>
2024-09-09 23:04:05 -0300

    refactor(tasks): separated async_post from task

--- api/tasks.py
+++ api/tasks.py
@@ -10,14 +10,16 @@ FHIR_ENDPOINT = os.getenv("FHIR_ENDPOINT", "http://localhost:8080/fhir")
 
 @shared_task(autoretry_for=(Exception,), retry_kwargs={'max_retries': 7, 'countdown': 10})
 def send_patient_to_fhir(patient_data):
-    async def async_post():
-        async with httpx.AsyncClient() as client:
-            try:
-                response = await client.post(f"{FHIR_ENDPOINT}/Patient", json=patient_data)
-                print(f"Sent to FHIR: {response.status_code}")
-            except Exception as e:
-                print("Connection Failed, Retrying in 10 seconds")
-                raise
-
     import asyncio
-    asyncio.run(async_post())
+    asyncio.run(async_post(patient_data))
+
+
+async def async_post(patient_data):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(f"{FHIR_ENDPOINT}/Patient", json=patient_data)
+            print(f"Sent to FHIR: {response.status_code}")
+            return await response.json()
+        except Exception as e:
+            print("Connection Failed, Retrying in 10 seconds")
+            raise
